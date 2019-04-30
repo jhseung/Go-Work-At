5f8e5bfd844dac354c60f32ced2b4d9ca05b42d1
Ji Hwan Seung <jiseung95@gmail.com>
2019-04-29 21:56:40 -0400

    Add company review link, pros, cons, and ui adjustments

--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -1,7 +1,8 @@
-from . import *  
+from . import *
+from app import app
 from app.irsystem.models.helpers import *
 from app.irsystem.models.helpers import NumpyEncoder as NumpyEncoder
-import sys, os
+import sys, os, json
 
 project_name = "Go Work At"
 net_id = "Ji Hwan Seung: js2687; Ju Un Park: jp848; Yong Jin Cho: yc628; Sang Won Yoon: sy623"
@@ -10,6 +11,12 @@ net_id = "Ji Hwan Seung: js2687; Ju Un Park: jp848; Yong Jin Cho: yc628; Sang Wo
 def search():
 	skillset_query = request.args.get('skillset')
 	company_quality_query = request.args.get('company_quality')
+
+	with open(os.path.join(app.root_path, "./backend/scraper/url_dict.json")) as f:
+		company_to_review_url = json.load(f)
+
+	with open(os.path.join(app.root_path, "./backend/keywords.json")) as f:
+		company_to_keyword = json.load(f)
 
 	print ("skillset query is: {}".format(skillset_query))
 	print ("company_quality is: {}".format(company_quality_query))
@@ -23,8 +30,14 @@ def search():
 		print (result['company_name'])
 		if result['company_name'] == 'accenture':
 			print ("PRINTING")
-	# print (results, file=sys.stderr)
-	return render_template('search.html', results=results)
+	print (company_to_review_url, file=sys.stderr)
+	print (company_to_keyword, file=sys.stderr)
+
+	return render_template('search.html', 
+						   results=results, 
+						   company_to_review_url=company_to_review_url,
+						   company_to_keyword=company_to_keyword
+						   )
 
 
 
